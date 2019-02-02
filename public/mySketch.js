5a84fffa359dbc0d7e3bde301b20cf160935ffce
3mrrrx <hasanmashni@hotmail.com>
2019-02-02 21:28:53 +0100

    update

--- public/mySketch.js
+++ public/mySketch.js
@@ -8,8 +8,21 @@ var sound6;
 var events = 0;
 var events_old = 0;
 
+var boton1;
+var boton2;
+var boton3;
+var boton4;
+var boton5;
+var boton6;
+var perfomanceCase=3;
+var alreadyExecute=false;
+
+
 var waitTime;
 var startTime;
+
+var finishTimeCase3;
+
 var sounds=[];
 var colours=[];
 
@@ -49,12 +62,12 @@ socket.on('click_count3',function(value) {counter3 = value;});
 socket.on('click_count4',function(value) {counter4 = value;});
 socket.on('click_count5',function(value) {counter5 = value;});
 
-// 
+//
 socket.on('events',function(value) {events = value;});
 
 
 function preload() {
-
+/*
  sound1 = new Audio('assets/Sound0.mp3');
  sound2 = new Audio('assets/Sound1.mp3');
  sound3 = new Audio('assets/Sound2.mp3');
@@ -67,7 +80,7 @@ function preload() {
  sound10 = new Audio('assets/Sound9.mp3');
  sound11 = new Audio('assets/Sound10.mp3');
  sound12 = new Audio('assets/Sound11.mp3');
-
+*/
  sounds[0] = new Audio('assets/Sound0.mp3');
  sounds[1] = new Audio('assets/Sound1.mp3');
  sounds[2] = new Audio('assets/Sound2.mp3');
@@ -77,17 +90,19 @@ function preload() {
  sounds[6] = new Audio('assets/Sound6.mp3');
  sounds[7] = new Audio('assets/Sound7.mp3');
  sounds[8] = new Audio('assets/Sound8.mp3');
+ sounds[9] = new Audio('assets/Sound9.mp3');  // musicBox
+/*
  sounds[9] = new Audio('assets/Sound9.mp3');
  sounds[10] = new Audio('assets/Sound10.mp3');
  sounds[11] = new Audio('assets/Sound11.mp3');
-
+*/
 }
 
 function setup() {
 createCanvas(windowWidth, windowHeight);
-background(100);
+background('#D7DCD7');
   startTime = 0;
-  waitTime = 15000; // 3 segundos
+  waitTime = 1500; // 3 segundos
 
 colours[0] = color('#3E78B2');
 colours[1] = color('#FA8334');
@@ -95,52 +110,191 @@ colours[2] = color('#976BAE');
 colours[3] = color('#F3CA40');
 colours[4] = color('#FE5F55');
 colours[5] = color('#70C1B3');
+
+boton1=false;
+boton2=false;
+boton3=false;
+boton4=false;
+boton5=false;
+boton6=false;
+
 }
 
 function draw() {
 //background(100);
-  //background(colours[ 1 ]);
+  //background(colours[ 1
+if(perfomanceCase==1){
 noStroke();
   // FIRST COLUMN
-fill(200,100,0);
+  //textFont('Gotham');
+//textAlign(CENTER);
+//textStyle(BOLD);
+if(boton1==false){
+  fill('#FFAD5C');
+}
+else{
+  console.log("here");
+  fill('#D18E4C');
+}
 rect( 10 ,0, width/2-20, height/3 );    // 0
 fill(0);
-  textSize(30);
-  text(counter0,width/4  ,height/4  );
-fill(200,100,100);
+textSize(30);
+text( "1" /*counter0*/,width/4  ,height/4  );
+//fill(200,100,100);
+if(boton2==false){
+  fill(' #FF7F00 ');
+}
+else{
+  fill('#D16800');
+}
 rect( 10 , height/3 , width/2-20, height/3 );    // 1
 fill(0);
-  textSize(30);
-  text(counter1,width/4  ,2*height/4  );
-fill(200,100,200);
+textSize(30);
+text("2"/*counter1*/,width/4  ,2*height/4  );
+//fill(200,100,200);
+if(boton3==false){
+  fill(' #BA5D00 ');
+}
+else{
+  fill('#AA5500');
+}
 rect( 10 , 2*height/3, width/2-20, height/3 );   // 2
 fill(0);
   textSize(30);
-  text(counter2,width/4  ,3*height/4  );
+  text("3" /*counter2*/,width/4  ,3*height/4  );
   // SECOND COLUMN
-  fill(200,100,0);
+  //fill('#59CD90');
+  if(boton4==false){
+    fill(' #B8A5D5 ');
+  }
+  else{
+    fill('#A896C2');
+  }
 rect( width/2+10 ,0, width/2-20, height/3 );     // 3
 fill(0);
   textSize(30);
-  text(counter3,3*width/4  ,height/4  );
-fill(200,100,100);
+  text("4" /*counter3*/,3*width/4  ,height/4  );
+fill('#4381C1');
+if(boton5==false){
+  fill(' #9478C0 ');
+}
+else{
+  fill(' #876EAF ');
+}
 rect( width/2+10 ,height/3, width/2-20, height/3 );   // 4
 fill(0);
   textSize(30);
-  text(counter4,3*width/4  ,2*height/4  );
-fill(200,100,200);
+  text("5"/*counter4*/,3*width/4  ,2*height/4  );
+//fill('#D86152');
+if(boton6==false){
+  fill(' #7D5AB2 ');
+}
+else{
+  fill(' #7252A2 ');
+}
 rect( width/2+10 ,2*height/3, width/2-20, height/3 );   // 5
 fill(0);
   textSize(30);
-  text(counter5,3*width/4  ,3*height/4  );
+  text("6"/*counter5*/,3*width/4  ,3*height/4  );
+}
 
+
+if(perfomanceCase==2){
+noStroke();
+  // FIRST COLUMN
+  //textFont('Gotham');
+//textAlign(CENTER);
+//textStyle(BOLD);
+if(boton1==false){
+  fill('#FFAD5C');
+}
+else{
+  console.log("here");
+  fill('#D18E4C');
+}
+rect( 10 ,0, width/2-20, height/3 );    // 0
+fill(0);
+textSize(30);
+text( "1" /*counter0*/,width/4  ,height/4  );
+//fill(200,100,100);
+if(boton2==false){
+  fill(' #FF7F00 ');
+}
+else{
+  fill('#D16800');
+}
+rect( 10 , height/3 , width/2-20, height/3 );    // 1
+fill(0);
+textSize(30);
+text("2"/*counter1*/,width/4  ,2*height/4  );
+//fill(200,100,200);
+if(boton3==false){
+  fill(' #BA5D00 ');
+}
+else{
+  fill('#AA5500');
+}
+rect( 10 , 2*height/3, width/2-20, height/3 );   // 2
+fill(0);
+  textSize(30);
+  text("3" /*counter2*/,width/4  ,3*height/4  );
+  // SECOND COLUMN
+  //fill('#59CD90');
+  if(boton4==false){
+    fill(' #B8A5D5 ');
+  }
+  else{
+    fill('#A896C2');
+  }
+rect( width/2+10 ,0, width/2-20, height/3 );     // 3
+fill(0);
+  textSize(30);
+  text("4" /*counter3*/,3*width/4  ,height/4  );
+fill('#4381C1');
+if(boton5==false){
+  fill(' #9478C0 ');
+}
+else{
+  fill(' #876EAF ');
+}
+rect( width/2+10 ,height/3, width/2-20, height/3 );   // 4
+fill(0);
+  textSize(30);
+  text("5"/*counter4*/,3*width/4  ,2*height/4  );
+//fill('#D86152');
+if(boton6==false){
+  fill(' #7D5AB2 ');
+}
+else{
+  fill(' #7252A2 ');
+}
+rect( width/2+10 ,2*height/3, width/2-20, height/3 );   // 5
+fill(0);
+  textSize(30);
+  text("6"/*counter5*/,3*width/4  ,3*height/4  );
+}
   //console.log("millis: "+parseInt(millis())+" TiempoInicio: "+parseInt(startTime));
+if(perfomanceCase==3){
+  background('#9478C0');
+  if(alreadyExecute==false){
+    finishTimeCase3=millis()+52000;
+    alreadyExecute=true;
+  }
+  if( finishTimeCase3 > millis()){
+    console.log("ENTRAAAA");
+    sounds[9].loop = false;
+    sounds[ 9 ].play();
+    alreadyExecute=true;
+  }
+
+  /*
   hours=hour();
   minutes=minute();
   seconds=second();
-  //console.log("hour: "+hours+" Minute: "+minutes+" Seconds: "+seconds);
-  if(hours>=18 && minutes>=27 /*&& seconds>=0*/){
-    //console.log("Here");
+
+  console.log("hour: "+hours+" Minute: "+minutes+" Seconds: "+seconds);
+  if(hours>=18 && minutes>=27 ){
+    console.log("Here");
     if (millis() - startTime > waitTime) {
       console.log(parseInt(secuenceNumber));
       sounds[ parseInt(secuenceNumber) ].play();
@@ -151,23 +305,39 @@ fill(0);
         secuenceNumber=0;
       }
       startTime = millis();
-	}
+   }
+  }
+  */
   }
 
-	if(events != events_old){
-		console.log("event has changed: " + events);
-		events_old = events;
-	}
+if(perfomanceCase==4){
+  background('#FF9F1C' );
+}
+if(perfomanceCase==5){
+  background( ' #2EC4B6' );
+}
+if(perfomanceCase==6){
+  background( '#A896C2' );
+}
+
+if(events != events_old){
+console.log("event has changed: " + events);
+events_old = events;
+}
 }
 
 function mousePressed(){
+
+
 // FIRST COLUMN
+
+if(perfomanceCase==1){
 if(mouseX>width/4 -75 &&  mouseX< width/4 +75 ){
 if(mouseY>height/4 -50 && mouseY< height/4 +50){
       sounds[0].loop = true;
       sounds[0].play();
 socket.emit('clicked0');//Emitting user click
-print("ENTRA1");
+boton1=true;
 }
 }
 if(mouseX>width/4 -75 &&  mouseX< width/4 +75 ){
@@ -175,7 +345,7 @@ if(mouseY>2*height/4 -50 && mouseY< 2*height/4 +50){
       sounds[1].loop = true;
       sounds[1].play();
 socket.emit('clicked1');//Emitting user click
-print("ENTRA2");
+boton2=true;
 }
 }
 if(mouseX>width/4 -75 &&  mouseX< width/4 +75 ){
@@ -183,7 +353,7 @@ if(mouseY>3*height/4 -50 && mouseY< 3*height/4 +50){
       sounds[2].loop = true;
       sounds[2].play();
 socket.emit('clicked2');//Emitting user click
-print("ENTRA3");
+boton3=true;
 }
 }
 // SECOND COLUMN
@@ -192,7 +362,7 @@ if(mouseY>height/4 -50 && mouseY< height/4 +50){
       sounds[3].loop = true;
       sounds[3].play();
 socket.emit('clicked3');//Emitting user click
-print("ENTRA4");
+boton4=true;
 }
 }
 if(mouseX>3*width/4 -75 &&  mouseX< 3*width/4 +75 ){
@@ -200,7 +370,7 @@ if(mouseY>2*height/4 -50 && mouseY< 2*height/4 +50){
       sounds[4].loop = true;
       sounds[4].play();
 socket.emit('clicked4');//Emitting user click
-print("ENTRA5");
+boton5=true;
 }
 }
 if(mouseX>3*width/4 -75 &&  mouseX< 3*width/4 +75 ){
@@ -208,9 +378,64 @@ if(mouseY>3*height/4 -50 && mouseY< 3*height/4 +50){
       sounds[5].loop = true;
       sounds[5].play();
 socket.emit('clicked5');//Emitting user click
-print("ENTRA6");
+boton6=true;
 }
 }
+}
+   ////// CASE TWO ////////
+if(perfomanceCase==2){
+if(mouseX>width/4 -75 &&  mouseX< width/4 +75 ){
+if(mouseY>height/4 -50 && mouseY< height/4 +50){
+      sounds[0].loop = true;
+      sounds[0].play();
+socket.emit('clicked0');//Emitting user click
+boton1=true;
+}
+}
+if(mouseX>width/4 -75 &&  mouseX< width/4 +75 ){
+if(mouseY>2*height/4 -50 && mouseY< 2*height/4 +50){
+      sounds[1].loop = true;
+      sounds[1].play();
+socket.emit('clicked1');//Emitting user click
+boton2=true;
+}
+}
+if(mouseX>width/4 -75 &&  mouseX< width/4 +75 ){
+if(mouseY>3*height/4 -50 && mouseY< 3*height/4 +50){
+      sounds[2].loop = true;
+      sounds[2].play();
+socket.emit('clicked2');//Emitting user click
+boton3=true;
+}
+}
+// SECOND COLUMN
+if(mouseX>3*width/4 -75 &&  mouseX< 3*width/4 +75 ){
+if(mouseY>height/4 -50 && mouseY< height/4 +50){
+      sounds[3].loop = true;
+      sounds[3].play();
+socket.emit('clicked3');//Emitting user click
+boton4=true;
+}
+}
+if(mouseX>3*width/4 -75 &&  mouseX< 3*width/4 +75 ){
+if(mouseY>2*height/4 -50 && mouseY< 2*height/4 +50){
+      sounds[4].loop = true;
+      sounds[4].play();
+socket.emit('clicked4');//Emitting user click
+boton5=true;
+}
+}
+if(mouseX>3*width/4 -75 &&  mouseX< 3*width/4 +75 ){
+if(mouseY>3*height/4 -50 && mouseY< 3*height/4 +50){
+      sounds[5].loop = true;
+      sounds[5].play();
+socket.emit('clicked5');//Emitting user click
+boton6=true;
+}
+}
+}
+
+
 }
 
 function mouseReleased(){
@@ -228,6 +453,12 @@ function mouseReleased(){
   sounds[5].pause();
   sounds[5].currentTime = 0;
 
+  boton1=false;
+  boton2=false;
+  boton3=false;
+  boton4=false;
+  boton5=false;
+  boton6=false;
 /*
   sound1.pause();
   sound1.currentTime = 0;
